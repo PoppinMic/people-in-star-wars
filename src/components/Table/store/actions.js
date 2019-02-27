dd8c17d563a30e83937aec0f9981f87d1e7eb40a
PoppinMic <cjma1990au@gmail.com>
2019-02-27 22:16:13 +1100

    delete old experiment files for refactoring

--- src/components/Table/store/actions.js
+++
@@ -1,6 +0,0 @@
-import { FETCH_PEOPLE_LIST } from './constants';
-
-export const getPeopleList = payload => ({
-  type: FETCH_PEOPLE_LIST,
-  payload
-});
--- src/store/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux-immutable';
-import { reducer as tableReducer } from '../components/Table/store';
-
-const reducer = combineReducers({
-	header: tableReducer
-});
-
-export default reducer;
