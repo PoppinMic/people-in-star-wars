e8d30e9648ad43344ef9b7cc80dbf42172411065
PoppinMic <cjma1990au@gmail.com>
2019-02-27 22:15:42 +1100

    defined actions and actionTypes

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,13 @@
+// const for pagination
+export const NEXT_PAGE = 'next_page';
+export const PREV_PAGE = 'prev_page';
+
+// const for people data
+export const REQ_PEOPLE = 'request_people';
+export const RES_PEOPLE = 'response_people';
+export const REQ_PEOPLE_FAIL = 'request_people_fail';
+
+// const for films data
+export const REQ_FILMs = 'request_films';
+export const RES_FILMS = 'response_films';
+export const REQ_FILMS_FAIL = 'request_films_fail';
---
+++ src/actions/index.js
@@ -0,0 +1,52 @@
+import {
+  REQ_PEOPLE,
+  RES_PEOPLE,
+  REQ_PEOPLE_FAIL,
+  REQ_FILMs,
+  RES_FILMS,
+  REQ_FILMS_FAIL,
+  NEXT_PAGE,
+  PREV_PAGE,
+} from './actionTypes';
+
+// People Actions
+export const requestPeople = () => ({
+  type: REQ_PEOPLE
+});
+
+export const responsePeople = (pageNum, payload) => ({
+  type: RES_PEOPLE,
+  pageNum,
+  payload
+});
+
+export const requestPeopleFail = error => ({
+  type: REQ_PEOPLE_FAIL,
+  error
+});
+
+// Film Actions
+export const requestFilms = () => ({
+  type: REQ_FILMs
+});
+
+export const responseFilms = filmsPayload => ({
+  type: RES_FILMS,
+  filmsPayload
+});
+
+export const requestFilmsFail = error => ({
+  type: REQ_FILMS_FAIL,
+  error
+});
+
+// Pagination
+export const nextPage = pageNum => ({
+  type: NEXT_PAGE,
+  pageNum
+});
+
+export const prevPage = pageNum => ({
+  type: PREV_PAGE,
+  pageNum
+});
