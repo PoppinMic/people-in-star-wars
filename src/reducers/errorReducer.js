9dd075bc1fb3921c54f2e47bb3fa3f57cf151614
PoppinMic <cjma1990au@gmail.com>
2019-03-04 00:46:36 +1100

    error reducer correction

--- src/reducers/errorReducer.js
+++ src/reducers/errorReducer.js
@@ -3,9 +3,9 @@ import { REQ_PEOPLE_FAIL, REQ_FILMS_FAIL } from '../actions/actionTypes';
 const errorReducer = (state = { errMs: null }, { type, error }) => {
   switch (type) {
     case REQ_PEOPLE_FAIL:
-      return { ...state, errMs: 'People data request failed: ', error };
+      return { ...state, errMs: `People data request failed: ${error}` };
     case REQ_FILMS_FAIL:
-      return { ...state, errMs: 'Films data request failed: ', error };
+      return { ...state, errMs: `Films data request failed: ${error}` };
 
     default:
       return state;
